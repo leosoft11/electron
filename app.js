0561eba0c1699ad89a8717b7d24f90600254c606
Anton Kuznetsov <lukarek@Antons-MacBook-Pro.local>
2021-10-23 00:43:02 +0400

    Добавил обновление приложения

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 
+require('update-electron-app')();
+
 const createWindow = () => {
     let win = new BrowserWindow({
         width: 800,
@@ -12,5 +14,6 @@ const createWindow = () => {
     })
     win.loadFile('index.html');
 }
+
 
 app.on('ready', createWindow);
